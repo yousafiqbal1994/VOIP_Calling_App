88e4e67dc5dd33b926ac3701412586aadf5f02d6
Yousaf Iqbal <yousafiqbal1994@gmail.com>
2017-07-23 01:31:45 +0500

    VOIP Calling App

---
+++ src/com/sinch/android/rtc/sample/calling/PlaceCallActivity.java
@@ -0,0 +1,113 @@
+package com.sinch.android.rtc.sample.calling;
+
+import com.sinch.android.rtc.calling.Call;
+
+import android.content.Context;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+public class PlaceCallActivity extends BaseActivity {
+
+    private Button mCallButton;
+    private EditText mCallName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        mCallName = (EditText) findViewById(R.id.callName);
+        mCallButton = (Button) findViewById(R.id.callButton);
+        mCallButton.setEnabled(false);
+        mCallButton.setOnClickListener(buttonClickListener);
+
+        Button stopButton = (Button) findViewById(R.id.stopButton);
+        stopButton.setOnClickListener(buttonClickListener);
+    }
+
+    @Override
+    protected void onServiceConnected() {
+        TextView userName = (TextView) findViewById(R.id.loggedInName);
+        userName.setText(getSinchServiceInterface().getUserName());
+        mCallButton.setEnabled(true);
+    }
+
+    @Override
+    public void onDestroy() {
+        if (getSinchServiceInterface() != null) {
+            getSinchServiceInterface().stopClient();
+        }
+        super.onDestroy();
+    }
+
+    private void stopButtonClicked() {
+        if (getSinchServiceInterface() != null) {
+            getSinchServiceInterface().stopClient();
+        }
+        finish();
+    }
+
+    private void callButtonClicked() {
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location lastLoc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        Double longitude = 71.4373181;
+        Double latitude = 33.9536705;
+
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses = null;
+        try {
+            addresses = geocoder.getFromLocation(latitude, longitude, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put("location", addresses.get(0).getAddressLine(1));
+
+        String userName = mCallName.getText().toString();
+        if (userName.isEmpty()) {
+            Toast.makeText(this, "Please enter a user to call", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        Call call = getSinchServiceInterface().callUser(userName, headers);
+        String callId = call.getCallId();
+
+        Intent callScreen = new Intent(this, CallScreenActivity.class);
+        callScreen.putExtra(SinchService.CALL_ID, callId);
+        startActivity(callScreen);
+    }
+
+    private OnClickListener buttonClickListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.callButton:
+                    callButtonClicked();
+                    break;
+
+                case R.id.stopButton:
+                    stopButtonClicked();
+                    break;
+
+            }
+        }
+    };
+}
